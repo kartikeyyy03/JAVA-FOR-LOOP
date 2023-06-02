17f2325d217e45ffcdb5274d3e5b79a9af0e3168
Kartikeya Kandpal <kandpalkartikeya93@gmail.com>
2023-06-03 01:05:15 +0530

    question 1

---
+++ question_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class question_1 {
+    public static void main(String[] args) {
+
+        int factor=0;
+        int A;
+
+        Scanner sc=new Scanner(System.in);
+        A=sc.nextInt();
+    
+    for(int i=1;i<=A;i++){
+
+        if(A%i==0){factor=factor+1;}
+    }
+
+    if(factor==2){System.out.println("YES");}
+    else{System.out.println("NO");}
+}
+
+
+}
