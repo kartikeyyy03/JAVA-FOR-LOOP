022176bcdb5ed148d28e8c0bd247d8e7cf30d9b1
Kartikeya Kandpal <kandpalkartikeya93@gmail.com>
2023-06-03 22:34:36 +0530

    Number of factors

---
+++ question_6.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class question_6 {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    int factor=0;
+
+    int N=sc.nextInt();
+
+    if(N<1||N>300){System.out.println("Invalid Input");}
+    else{
+
+    for(int i=1;i<=N;i++){
+
+        if(N%i==0){
+            factor=factor+1;
+        }
+
+    }
+
+    
+    System.out.println(factor);
+
+
+    }
+}
+
+    
+}
